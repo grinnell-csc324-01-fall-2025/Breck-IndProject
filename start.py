d6c4c5326b85816aa3118ebbf0f15ab64d65eeb5
Breck Bishop <118920335+bbish30@users.noreply.github.com>
2025-09-15 20:06:16 -0700

    Add files via upload

--- start.py
+++ start.py
@@ -1,5 +1,11 @@
-import torch, torchvision
+import torch
+import torch.nn as nn
+import os
+from torch.utils.data import DataLoader
+import numpy as np
+from chess import Board
 
 # Hello world type to show how it works
 x = torch.rand(5, 3)
 print(x)
+
